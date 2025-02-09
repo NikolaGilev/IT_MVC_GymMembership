773099cd130c8b003a8d83243c0218666d6e6871
nikola.gilev <nikola.gilev@codechem.com>
2025-02-10 00:12:36 +0100

    update script

--- kubernetes/run.sh
+++ kubernetes/run.sh
@@ -1,2 +1,8 @@
+minikube start 
+kubectl get nodes
+kubectl create namespace gym-app
+
 kubectl apply -f kubernetes/
 kubectl get all -n gym-app
+kubectl logs -n gym-app -l app=gym-app
+# kubectl port-forward svc/gym-app-service 8080:80 -n gym-app
